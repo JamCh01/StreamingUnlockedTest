6dcbe3758b4f9388b692b344ce668d299b0d574a
JamCh01 <me@jamchoi.cc>
2020-12-11 20:41:13 +0800

    feat(plugin): 完成动画疯插件 (#7)
    Co-authored-by: JamCh01 <cuijiacheng@seclover.com>

--- sut/plugins.py
+++ sut/plugins.py
@@ -112,3 +112,21 @@ class Kktv(PluginFather):
             return True
         else:
             return False
+
+
+class Gamer(PluginFather):
+    start_url = "https://ani.gamer.com.tw/animeVideo.php?sn=19526"
+
+    def request(self, page: Page, login: login_typing = None) -> str:
+        page.fetch(self.start_url)
+        return page.html()
+
+    def check(self, page_source: str) -> bool:
+        soup = BeautifulSoup(page_source, "lxml")
+        check_node = soup.find(
+            "div", {"class": "vjs-modal-dialog-content"}
+        ).text.strip()
+        if check_node == "本服務授權範圍僅限台灣地區，如果您為外國用戶或使用變更IP的插件或軟體，將無法正常使用本服務。":
+            return False
+        else:
+            return True
