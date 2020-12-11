1ca09ad3e47ec0192f256bd41ca29fe337863e77
JamCh01 <me@jamchoi.cc>
2020-12-11 20:04:36 +0800

    feat(plugin): 增加 kktv 插件 (#6)
    Co-authored-by: JamCh01 <cuijiacheng@seclover.com>

--- sut/plugins.py
+++ sut/plugins.py
@@ -92,7 +92,22 @@ class AbemaTV(PluginFather):
         check_node = soup.find(
             "div", {"class": "c-vod-EpisodePlayerContainer-reject-by-region"}
         )
-        print(check_node)
+        if isinstance(check_node, type(None)):
+            return True
+        else:
+            return False
+
+
+class Kktv(PluginFather):
+    start_url = "https://www.kktv.me/play/01000406010001"
+
+    def request(self, page: Page, login: login_typing = None) -> str:
+        page.fetch(self.start_url)
+        return page.html()
+
+    def check(self, page_source: str) -> bool:
+        soup = BeautifulSoup(page_source, "lxml")
+        check_node = soup.find("h2", {"class": "forbidden__message warning kktv"})
         if isinstance(check_node, type(None)):
             return True
         else:
